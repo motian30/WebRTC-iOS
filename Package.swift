b662af26aee234680eece117fec251d3a6cc8de4
sq-0523 <sq-0523>
2025-11-26 17:14:47 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+
+let package = Package(
+    name: "WebRTC",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .macCatalyst(.v14),
+    ],
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/webrtc-sdk/Specs/releases/download/137.7151.09/WebRTC.xcframework.zip",
+            checksum: "2e6c9ee94ff109f4a9e8a7c0680fd6a3d7f88efc6abeedb1414973eca7e41b0c"
+        ),
+    ]
+)
